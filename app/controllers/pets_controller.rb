fcfdf147f45dd946963f8634b1bc4123e3d6a251
Peter Mackay <project2501@gmail.com>
2019-05-20 15:25:29 -0600

    pets controller implemented

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,51 @@
 class PetsController < ApplicationController
+  before_action :set_pet, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pets = current_user.pets
+  end
+
+  def show
+  end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to pets_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pet.update(pet_params)
+      redirect_to pet_path(@pet)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @pet.destroy
+    redirect_to pets_path
+  end
+
+  private
+
+  def set_pet
+    @pet = current_user.pets.find(params[:id])
+  end
+
+  def pet_params
+    params.require(:pet).permit(:name, :animal)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'pets/index'
-  get 'pets/show'
-  get 'pets/new'
-  get 'pets/edit'
   devise_for :users
   root "pets#index"
+
+  resources :pets
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
